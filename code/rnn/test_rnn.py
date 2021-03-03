eed94618fe2021d4a056ce8b876c9c9f34fd7b54
1801573781 <1801573781@qq.com>
2021-03-04 07:47:54 +0800

    rnn

--- code/rnn/test_rnn.py
+++ code/rnn/test_rnn.py
@@ -4,16 +4,12 @@ Author：lzb
 Date：2021.02.15
 """
 
-import os
 
-from activation.last_hop_activation import LastHopActivation, SoftMaxLHA
+from activation.last_hop_activation import SoftMaxLHA
 from activation.normal_activation import Sigmoid, ReLU
 
-from loss.loss import MSELoss, CrossEntropyLoss
-
-from bp import bp_nn
-from rnn import recurrent_nn, rnn_poet
-from sample.image_softmax_sample import ImageSoftMaxSample
+from loss.loss import CrossEntropyLoss
+from rnn import rnn_poet
 from sample.rnn_sample import RNNSanmple
 
 
@@ -41,9 +37,6 @@ def test_poem():
 
     sample.create_sample()
 
-    # train_sx_list = sample.get_sx_list()
-    # train_sy_list = sample.get_sy_list()
-
     train_sx_group = sample.get_sx_group()
     train_sy_group = sample.get_sy_group()
 
@@ -59,7 +52,6 @@ def test_poem():
     rate = 0.01
 
     # 训练
-    # nn.train(train_sx_list, train_sy_list, loop_max, neuron_count_list, rate)
     nn.train(train_sx_group, train_sy_group, loop_max, neuron_count_list, rate)
 
     # 4. 测试
@@ -68,16 +60,14 @@ def test_poem():
     ch = "白"
     test_sx = sample.create_test_sample(ch)
 
-    # 测试
+    # 4.2 测试
     py_list = list()
-    # nn.predict_r(test_sx, py_list)
     nn.predict_recurrent(test_sx, py_list)
 
-    # 将测试样本放在首位，这样就组成了一首完整的诗
+    # 4.3 将测试样本放在首位，这样就组成了一首完整的诗
     py_list.insert(0, ch)
 
     print("\n")
     print("py:\n")
 
     print(py_list)
-
